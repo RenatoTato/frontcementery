74b2679a9210f67caf2a1723d482e8d54512db18
Renato <renatodaniel.cb@gmail.com>
2024-12-22 00:09:45 -0500

    areglos

--- cementery/src/app/app.component.ts
+++ cementery/src/app/app.component.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-import { RouterOutlet, Router } from '@angular/router';
+import { RouterOutlet } from '@angular/router';
 import { FormsModule } from '@angular/forms';
 import { CommonModule } from '@angular/common';
 
